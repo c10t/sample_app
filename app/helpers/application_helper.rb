e7f5adec36715f6d9416be9e30c8eaaf7224ff50
c10t <rem_tate@yahoo.co.jp>
2017-07-05 14:49:00 +0000

    commented annotations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,16 @@
 module ApplicationHelper
   
   # Returns the full title on a per-page basis.
+  # Method def, optional arg
   def full_title(page_title = '')
+    # Variable assignment
     base_title = "Ruby on Rails Tutorial Sample App"
+    # Boolean test
     if page_title.empty?
+      # Implicit return
       base_title
     else
+      # String concatenation
       page_title + " | " + base_title
     end
   end
